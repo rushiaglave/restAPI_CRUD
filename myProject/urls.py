99bc8e93861931981fab1739ee965b952268bd7d
Rushi_Aglave <rushiaglave1010@gmail.com>
2024-05-13 11:04:18 +0530

    commit 13/5/2024

--- myProject/urls.py
+++ myProject/urls.py
@@ -5,6 +5,7 @@ from webapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('task-list/', views.tasklist, name='task-list'),
+    path('task-listid/<str:pk>/', views.tasklistid, name='task-list'),
     path('task-create/', views.taskcreate, name='task-create'),
     path('task-update/<str:pk>/', views.taskupdate, name='task-update'),
     path('task-delete/<str:pk>/', views.taskdelete, name='task-delete')
--- webapp/views.py
+++ webapp/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import employees
 from .serializers import employeesSerializer
+from rest_framework import status
 
 # Create your views here.
 # creating the GET request here to extract the data
@@ -10,6 +11,16 @@ def tasklist(request):
         employees1 = employees.objects.all()
         Serializer =employeesSerializer(employees1, many=True)
         return Response(Serializer.data)
+
+@api_view(['GET'])
+def tasklistid(request, pk):
+    try:
+        employee = employees.objects.get(emp_id=pk)
+    except employees.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    serializer = employeesSerializer(employee)
+    return Response(serializer.data)
 
 #  creating the POST request here to update the data   
 @api_view(['POST'])
